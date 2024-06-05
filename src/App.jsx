6306e6442f7a64137fa88b4f4a4172a7847de8c8
Francis <karma.police190@gmail.com>
2024-06-05 02:18:20 -0500

    Clase 1 react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import LeftSide from './components/LeftSide/LeftSide'
+function App() {
+  
+  return (
+    
+        <div className="conteiner">
+            <LeftSide/>
+            <main>
+
+            </main>
+        </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/LeftSide/LeftSide.jsx
@@ -0,0 +1,22 @@
+import UserItem from '../UserItem/UserItem'
+import './styles.css'
+function LeftSide() {
+    return (
+        <aside>
+            <h1>Usuarios</h1>
+             <ul>
+                <UserItem/>
+                <UserItem/>
+                <UserItem/>
+                <UserItem/>
+                <UserItem/>
+                <UserItem/>
+                <UserItem/>
+                
+            </ul>
+
+      </aside>
+   )
+}
+
+export default LeftSide
